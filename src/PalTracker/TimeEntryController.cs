32adcfd94d981be920e402cb1a02099220e36202
Mayur <mshah4@humana.com>
2019-03-12 08:42:43 -0600

    added new classes

---
+++ src/PalTracker/TimeEntryController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace PalTracker
+{
+
+   
+
+        [Route("/")]
+        public class TimeEntryContoller : ControllerBase
+        {
+            
+
+            private readonly TimeEntry _timesheet;
+     
+
+            [HttpGet]
+            public long? Id() => _timesheet.Id;
+            public long? ProjectId() => _timesheet.ProjectId;
+            public long? UserId() => _timesheet.UserId;
+            public DateTime  Date() => _timesheet.Date;
+            public int Hours() => _timesheet.Hours;
+
+
+            public TimeEntryContoller(TimeEntry timesheet)
+            {
+                 try{
+                
+                   _timesheet.Id = timesheet.Id;
+                   _timesheet.ProjectId = timesheet.ProjectId;
+                   _timesheet.UserId = timesheet.UserId;
+                   _timesheet.Date = timesheet.Date ;
+                   _timesheet.Hours = timesheet.Hours;
+                   
+
+                 }
+                   catch{
+                    throw new NotImplementedException();
+                        }
+
+            }
+    }
+}
+ 
+    
+    
+
